b908049d3180b3ad6c1867faa61750b645601d82
veerark <veeraraghavan.kannan@outlook.com>
2015-01-12 15:28:05 +0530

    tt

---
+++ TestJava/src/com/test/TestJava.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Veera");
+<<<<<<< HEAD
+		System.err.println("Veera 22");
+=======
+>>>>>>> refs/heads/master
+	}
+
+}
+
